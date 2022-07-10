d4c1ef97659688f3e9db82bfd7c6f4b21b37eab9
Ellis O'Dowd <ellisodowd@pop-os.localdomain>
2022-07-10 00:50:07 -0700

    0.0.6.2 added authorization to users

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const validateRegisterInput = require("../validation/registerValidation");
+const jwt = require("jsonwebtoken");
+const requiresAuth = require("../middleware/permissions");
 //@route GET /api/auth/test
 //@desc Test the auth route
 //@acess Public
@@ -46,13 +48,70 @@ router.post("/register", async (req, res) => {
     //save user to db
     const savedUser = await newUser.save();
 
+    const userToReturn = { ...savedUser._doc };
+    delete userToReturn.password;
     // return the new used from the db
-    return res.json(savedUser);
+    return res.json(userToReturn);
   } catch (err) {
     console.log(err);
     res.status(500).send(err.message); //show error
   }
   res.send("Auth route working");
+});
+
+//@route POST /api/auth/login
+//@desc Login user and return an access token
+//@acess Public
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: new RegExp("^" + req.body.email + "$", "i"),
+    });
+    if (!user) {
+      return res.status(400).json({
+        error: "Either your email or password (or both) was incorrect",
+      });
+    }
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!passwordMatch) {
+      return res.status(400).json({
+        error: "Either your email or password (or both) was incorrect",
+      });
+    }
+
+    const payload = { userId: user._id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      //verify payload with the env.jwtsecret
+      expiresIn: "7d",
+    });
+
+    res.cookie("access-token", token, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), //a week
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", //if in prodction, secure will be true
+    });
+
+    const userToReturn = { ...user._doc };
+    delete userToReturn.password;
+
+    return res.json({ token: token, user: userToReturn });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  }
+});
+
+//@route GET /api/auth/current
+//@desc Return currently authed user
+//@acess Private
+router.get("/current", requiresAuth, (req, res) => {
+  if (!req.user) {
+    return res.status(401).send("Unauthorized");
+  }
+  return res.json(req.user);
 });
 
 module.exports = router;
--- src/components/pages/AccountCreation.js
+++ src/components/pages/AccountCreation.js
@@ -1,10 +1,85 @@
 import React from "react";
-
+import { useState } from "react";
+import eyeicon from "../../eyeicon.png";
+import eyestrike from "../../eyestrike.png";
 const AccountCreation = () => {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVis = () => {
+    setPasswordShown(!passwordShown);
+  };
   return (
-    <div>
-      <h1> account creation page</h1>
-    </div>
+    <>
+      <h1 className="LogoText font-patua LogoSize centered-div">Mynt</h1>
+      <div className="container top-margin">
+        <form>
+          <div className="left">
+            <h1 className="font-patua"> Login info</h1>
+
+            <input
+              className="input"
+              type="text"
+              name="name"
+              placeholder="email"
+            />
+
+            <div>
+              <input
+                className="input vertical-spacing"
+                type={passwordShown ? "text" : "password"}
+                password="password"
+                placeholder="password"
+              />
+
+              <input
+                className="input vertical-spacing"
+                type={passwordShown ? "text" : "password"}
+                password="password"
+                placeholder="confirm password"
+              />
+
+              <img
+                className="eyelogo"
+                src={passwordShown ? eyeicon : eyestrike}
+                alt="eye"
+                onClick={togglePasswordVis}
+              ></img>
+            </div>
+          </div>
+          <div className="right">
+            <h1 className="font-patua">Profile info</h1>
+            <div>
+              <div>
+                <input
+                  className="input"
+                  type="text"
+                  name="name"
+                  placeholder="@mynt"
+                />
+              </div>
+              <div className="vertical-spacing">
+                <input
+                  className="input"
+                  type="text"
+                  name="name"
+                  placeholder="Ellis Mynt"
+                />
+              </div>
+              <div className="vertical-spacing">
+                <input
+                  className="input"
+                  type="date"
+                  name="name"
+                  placeholder="Date of birth"
+                />
+              </div>
+              <div>
+                <input type="submit" className="button-log vertical-spacing" />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
